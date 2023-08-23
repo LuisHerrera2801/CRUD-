d9b02435fdc8db802fd2e0921e03d7242b643b5a
LuisHerrera2801 <luisherrera280101@gmail.com>
2023-08-22 18:51:42 -0600

    Se desplego la informacion de los servicios usando Axios

--- open311-front/src/pages/Home.js
+++ open311-front/src/pages/Home.js
@@ -1,35 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+    const [services,setServices]=useState([])
+
+    useEffect(()=>{
+      loadServices();
+    },[]);
+
+    const loadServices=async()=>{
+        const result=await axios.get("http://localhost:8081/services")
+        setServices(result.data);
+    }
+
   return (
     <div className='container'>
         <div className='py-4'>
         <table className="table border shadow">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">No Registro</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Categoria</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">Ubicación</th>
+      <th scope="col">Editar</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+
+    {
+      services.map((service,index)=>(
+        <tr>
+      <th scope="row" key={index}>{index+1}</th>
+      <td>{service.name}</td>
+      <td>{service.category}</td>
+      <td>{service.description}</td>
+      <td>{service.location}</td>
     </tr>
+      ))
+    }
+    
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      
     </tr>
   </tbody>
 </table>
